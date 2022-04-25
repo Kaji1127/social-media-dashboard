da8b6617da1c1603bc81476b0ba7d33201f81226
K.Kishin <riesgraf.birsack.1226@gmail.com>
2022-04-25 18:46:06 +0900

    export components

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Follower } from './Follower/Follower';
+export { default as FacebookOverview } from './FacebookOverview/FacebookOverview';
+export { default as HeaderTitle } from './HeaderTitle/HeaderTitle';
+export { default as Info } from './Info/Info';
+export { default as InstagramOverview } from './InstagramOverview/InstagramOverview';
+export { default as Likes } from './Likes/Likes';
+export { default as ToggleButton } from './ToggleButton/ToggleButton';
+export { default as TopBg } from './TopBg/TopBg';
+export { default as TwitterOverview } from './TwitterOverview/TwitterOverview';
+export { default as YoutubeOverview } from './YoutubeOverview/YoutubeOverview';
